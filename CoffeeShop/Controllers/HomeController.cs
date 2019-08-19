b2805d98f98b49a2c5dd34d91f25df9736ec77c8
Jake <jplane91@gmail.com>
2019-08-19 09:03:04 -0400

    first commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyAction(string button)
+        {
+            return View("Registration");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View(new PersonModel());
+        }
+
+
+        [HttpPost]
+        public IActionResult Registration(PersonModel newUser)
+        {
+            return RedirectToAction("EnrollUser", newUser);
+        }
+        public IActionResult EnrollUser(PersonModel user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ CoffeeShop/Models/PersonInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace CoffeeShop.Models
+{
+    public class PersonModel
+    {
+        [Display(Name = "First Name")]
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0 - 9a - zA - Z][-\w] *[0 - 9a - zA - Z] *\.)+[a-zA-Z0-9]{2,17})$")]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(30, ErrorMessage = "Must be between 3 and 30 characters", MinimumLength = 3)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
